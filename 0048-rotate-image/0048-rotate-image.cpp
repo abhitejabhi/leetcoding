ec8278816e4551ae66366196476e4092e41f639c
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-02-07 22:35:45 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (40.66%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>>ans(n,vector<int>(n,0));
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix.size();j++)
+            {
+                ans[j][n-i-1]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                matrix[i][j]=ans[i][j];
+            }
+        }
+    }
+};
