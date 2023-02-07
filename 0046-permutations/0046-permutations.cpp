c0cbf47ed65f88645ed818e14fbfd97db802a69e
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-02-07 20:58:54 +0530

    Time: 4 ms (55.14%), Space: 8.1 MB (32.25%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    void function(vector<int>ds,vector<vector<int>>&ans,vector<int>nums,int arr[])
+    void function(int ind,vector<int>nums,vector<vector<int>>&ans)
     {
-        if(ds.size()==nums.size())
+        if(ind==nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return ;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(arr[i]==0)
-            {
-                arr[i]=1;
-                ds.push_back(nums[i]);
-                function(ds,ans,nums,arr);
-                arr[i]=0;
-                ds.pop_back();
-            }
+            swap(nums[ind],nums[i]);
+            function(ind+1,nums,ans);
+            
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        int arr[nums.size()];
-        for(int i=0;i<nums.size();i++)
-        {
-            arr[i]=0;
-        }
-        function(ds,ans,nums,arr);
+        function(0,nums,ans);
         return ans;
     }
 };
