ca297857304485cf22ef4306e74e750d5774290b
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-07-27 20:57:21 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (42.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int rec(vector<int>& nums,int idx,vector<int>&dp){
-       if(idx >= nums.size()) return 0;
-       if(dp[idx] != -1) return dp[idx];
-       return dp[idx] = max(rec(nums, idx+1, dp), nums[idx] + rec(nums, idx+2, dp));
-  }
-  int rob(vector<int>& nums) {
-      vector<int>dp(nums.size()+1,-1);
-      return rec(nums,0,dp);
-  }
+    int f(vector<int>&nums,int idx,vector<int>&dp)
+    {
+        if(idx>=nums.size())return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        int steal=nums[idx]+f(nums,idx+2,dp);
+        int notsteal=f(nums,idx+1,dp);
+        return dp[idx]=max(steal,notsteal);
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return f(nums,0,dp);
+    }
 };
