9a3101b0650ee35951441173f9a6395b95112d04
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-07-27 10:28:50 +0530

    Time: 17 ms (43.75%), Space: 7.6 MB (63.80%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-     int nthUglyNumber(int n) {
-        vector <int> results (1,1);
-        int i = 0, j = 0, k = 0;
-        while (results.size() < n)
-        {
-            results.push_back(min(results[i] * 2, min(results[j] * 3, results[k] * 5)));
-            if (results.back() == results[i] * 2) ++i;
-            if (results.back() == results[j] * 3) ++j;
-            if (results.back() == results[k] * 5) ++k;
+    int nthUglyNumber(int n) {
+        vector<int> t(n+1);
+        //t[i] = ith Ugly number;
+        //we will reutrn t[n] = nth ugly number
+        
+        t[1] = 1; //1st Ugly number
+        
+        int i2; //i2th Ugly number
+        int i3; //i3rd Ugly number
+        int i5; //i5th Ugly number
+        
+        //initially i2th, i3rd and i5th Ugly number point to 1 i.e. first ugly number
+        i2 = i3 = i5 = 1;
+        
+        for(int i = 2; i<=n; i++) {
+            int i2th_ugly = t[i2] * 2;
+            
+            int i3rd_ugly = t[i3] * 3;
+            
+            int i5th_ugly = t[i5] * 5;
+            
+            t[i] = min({i2th_ugly, i3rd_ugly, i5th_ugly});
+            
+            if(t[i] == i2th_ugly)
+                i2++;
+            
+            if(t[i] == i3rd_ugly)
+                i3++;
+            
+            if(t[i] == i5th_ugly)
+                i5++;
         }
-        return results.back();
+        
+        return t[n];
     }
 };
