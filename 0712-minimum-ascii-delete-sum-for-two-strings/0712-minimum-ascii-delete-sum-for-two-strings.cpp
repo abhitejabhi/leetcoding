f95fe7ba25443337658d43a6c4e6f3c689dfa8a5
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-07-31 10:32:32 +0530

    Time: 48 ms (65.73%), Space: 15.2 MB (41.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    int f(string &s1, string &s2, int i, int j,vector<vector<int>>&dp) {
-        if (i < 0 && j < 0) return 0;
-        if (i < 0) {
-            int sum = 0;
-            while (j >= 0) {
-                sum += static_cast<int>(s2[j]);
-                j--;
+    int f(int i,int j,string &s1, string &s2,vector<vector<int>>&dp){
+        int n1 = s1.size(),n2 = s2.size(),sum = 0;
+        if(i == n1 and j == n2){
+            return 0;
+        }
+        else if(i == n1){
+            for(int k = j;k<n2;k++){
+                sum+=s2[k];
             }
             return sum;
         }
-        if (j < 0) {
-            int sum = 0;
-            while (i >= 0) {
-                sum += static_cast<int>(s1[i]);
-                i--;
+        else if(j == n2){
+            for(int k = i;k<n1;k++){
+                sum+=s1[k];
             }
             return sum;
         }
-        if(dp[i][j]!=-1)return dp[i][j];
-
-        if (s1[i] == s2[j]) {
-            return dp[i][j]=f(s1, s2, i - 1, j - 1,dp);
+        else if(dp[i][j] != -1){
+            return dp[i][j];
         }
-
-        int fir = f(s1, s2, i - 1, j,dp) + static_cast<int>(s1[i]);
-        int sec = f(s1, s2, i, j - 1,dp) + static_cast<int>(s2[j]);
-        return dp[i][j]=min(fir, sec);
+        
+        else if(s1[i] == s2[j]){
+            return  dp[i][j]=f(i+1,j+1,s1,s2,dp);
+            
+        }
+        else{
+           int t2 = s1[i] + f(i+1,j,s1,s2,dp);
+           int t3 = s2[j] + f(i,j+1,s1,s2,dp);
+            return dp[i][j]= min(t2,t3);
+        }
+        
+        
     }
-
     int minimumDeleteSum(string s1, string s2) {
-        int i = s1.size() - 1;
-        int j = s2.size() - 1;
-        vector<vector<int>>dp(i+1,vector<int>(j+1,-1));
-        return f(s1, s2, i, j,dp);
+        int n1 = s1.size(),n2 = s2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
+        return f(0,0,s1,s2,dp);
     }
 };
