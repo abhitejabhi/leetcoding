4438968af8917b5e61861b09598af3cdec28fa89
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-08-02 23:20:36 +0530

    Time: 3 ms (67.29%), Space: 7.8 MB (39.26%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    unordered_set<int> st;
     int n;
     
-    void solve(vector<int>& temp, vector<int>& nums) {
+    void solve(int idx, vector<int>& nums) {
 
-        if(temp.size() == n) {
-            result.push_back(temp);
+        if(idx == n) {
+            result.push_back(nums);
             return;
         }
         
-        for(int i = 0; i<n; i++) {
-            if(st.find(nums[i]) == st.end()) {
-                temp.push_back(nums[i]);
-                st.insert(nums[i]);
-
-                solve(temp, nums);
-
-                st.erase(nums[i]);
-                temp.pop_back();
-            }
+        for(int i = idx; i < n; i++) {
+            swap(nums[i], nums[idx]);
+            
+            solve(idx+1, nums);
+            
+            swap(nums[i], nums[idx]);
         }
         
         
@@ -29,9 +24,7 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         n = nums.size();
         
-        vector<int> temp;
-        
-        solve(temp, nums);
+        solve(0, nums);
         
         return result;
     }
