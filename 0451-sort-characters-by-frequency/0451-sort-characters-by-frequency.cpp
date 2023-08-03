f816db120ff62cfda1ff5ecfe4c275f62a568658
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-08-03 19:21:56 +0530

    Time: 11 ms (74.71%), Space: 8.4 MB (44.80%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    static bool comp(pair<char,int>p1,pair<char,int>p2)
+    {
+        return p1.second>p2.second;
+    }
+    string frequencySort(string s) {
+        map<char, int> mp;
+        for (auto &it : s) {
+            mp[it]++;
+        }
+        
+        // Create a vector of pairs from the map
+        vector<pair<char, int>> freqPairs;
+        for (auto it : mp) {
+            freqPairs.push_back(it);
+        }
+        
+        // Custom sorting based on frequency in descending order
+       sort(freqPairs.begin(),freqPairs.end(),comp);
+        
+        // Build the result string based on sorted frequency
+        string ans = "";
+        for (auto it : freqPairs) {
+            for (int i = 0; i < it.second; i++) {
+                ans += it.first;
+            }
+        }
+        
+        return ans;
+    }
+};
