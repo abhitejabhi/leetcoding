714f4cbe7e8fe66b5e68b67aa2dd78de6319c359
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2023-08-27 16:24:07 +0530

    Time: 8 ms (53.48%), Space: 6.7 MB (67.35%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        // handle edge-case where the product is 0
-        if (num1 == "0" || num2 == "0") return "0";
-        
-        // num1.size() + num2.size() == max no. of digits
-        vector<int> num(num1.size() + num2.size(), 0);
-        
-        // build the number by multiplying one digit at the time
-        for (int i = num1.size() - 1; i >= 0; --i) {
-            for (int j = num2.size() - 1; j >= 0; --j) {
-                num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-                num[i + j] += num[i + j + 1] / 10;
-                num[i + j + 1] %= 10;
+        int n = num1.size();
+        int m = num2.size();
+        if (num1 == "0" || num2 == "0") {
+            return "0"; // Return string "0" instead of integer 0
+        }
+        vector<int> temp(n + m, 0);
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                temp[i + j + 1] += (num1[j] - '0') * (num2[i] - '0');
+                temp[i + j] += temp[i + j + 1] / 10; // Add carry to the previous digit
+                temp[i + j + 1] %= 10; // Update the current digit
             }
         }
-        
-        // skip leading 0's
         int i = 0;
-        while (i < num.size() && num[i] == 0) ++i;
-        
-        // transofrm the vector to a string
+        while (i < temp.size() && temp[i] == 0) i++;
         string res = "";
-        while (i < num.size()) res.push_back(num[i++] + '0');
-        
+        while (i < temp.size()) {
+            res.push_back(temp[i] + '0');
+            i++;
+        }
         return res;
     }
 };
