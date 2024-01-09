9660720a224cc8d6db429bc357f8d13388b58480
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2024-01-09 20:25:08 +0530

    Time: 162 ms (21.40%), Space: 66.9 MB (47.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    bool possible(vector<int> &arr, int day, int m, int k) {
-    int n = arr.size(); //size of the array
-    int cnt = 0;
-    int noOfB = 0;
-    // count the number of bouquets:
-    for (int i = 0; i < n; i++) {
-        if (arr[i] <= day) {
-            cnt++;
-        }
-        else {
-            noOfB += (cnt / k);
-            cnt = 0;
-        }
-    }
-    noOfB += (cnt / k);
-    return noOfB >= m;
-}
-    int minDays(vector<int>& arr, int m, int k) {
-         long long val = m * 1ll * k * 1ll;
-    int n = arr.size(); //size of the array
-    if (val > n) return -1; //impossible case.
-    //find maximum and minimum:
-    int mini = INT_MAX, maxi = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        mini = min(mini, arr[i]);
-        maxi = max(maxi, arr[i]);
+    int f(int mid, vector<int>& arr, int m, int k) {
+        int n = arr.size();
+        int cnt = 0, temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= mid) {
+                temp++;
+            } else {
+            cnt += (temp / k);
+            temp= 0;
+            }
+        } 
+        cnt += (temp / k);
+        return (cnt >= m);
     }
 
-    //apply binary search:
-    int low = mini, high = maxi;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (possible(arr, mid, m, k)) {
-            high = mid - 1;
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int low = 1, high = 1e9;
+        int ans = -1;
+    long long temp=(long)k*(long)m;
+        if (temp > bloomDay.size()) return -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (f(mid, bloomDay, m, k)) {
+                ans = mid;
+                high = mid - 1; // Update high as a possible smaller day
+            } else {
+                low = mid + 1;
+            }
         }
-        else low = mid + 1;
+        return ans;
     }
-    return low;
-}
-    
 };
