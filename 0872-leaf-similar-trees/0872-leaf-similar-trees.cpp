0870830097adde56661bf928489f74fcd710f5e7
abhitejabhi <108194209+abhitejabhi@users.noreply.github.com>
2024-01-11 20:43:40 +0530

    Time: 0 ms (100.00%), Space: 13.1 MB (97.38%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,21 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void f(TreeNode* root, vector<int>& ans1) {
-        if (root) {
-            if (!root->left && !root->right) {
-                ans1.push_back(root->val);
-                return;
-            }
-            f(root->left, ans1);
-            f(root->right, ans1);
+    void f(TreeNode*root,vector<int>&temp)
+    {
+        if(root==NULL)return ;
+        if(root->left==NULL&&root->right==NULL)
+        {
+            temp.push_back(root->val);
         }
+        f(root->left,temp);
+        f(root->right,temp);
     }
-
+    
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans1;
-        vector<int> ans2;
-        f(root1, ans1);
-        f(root2, ans2);
-        return ans1 == ans2;
+        vector<int>temp1,temp2;
+        f(root1,temp1);
+        f(root2,temp2);
+        return (temp1==temp2);
     }
 };
